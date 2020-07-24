f11181ee5e3f56ee0db290850878c2f91a84f91b
Eric Wang <45348661+ericstar20@users.noreply.github.com>
2020-07-23 21:31:32 -0500

    Add files via upload

---
+++ yolo_video.py
@@ -0,0 +1,186 @@
+# import the necessary packages
+from collections import deque
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import os
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True,
+	help="path to input video")
+ap.add_argument("-o", "--output", required=True,
+	help="path to output video")
+ap.add_argument("-y", "--yolo", required=False,
+	help="base path to YOLO directory")
+# Tracking the baskectball center position
+ap.add_argument("-b", "--buffer", type=int, default=30,
+	help="max buffer size")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+ap.add_argument("-t", "--threshold", type=float, default=0.3,
+	help="threshold when applyong non-maxima suppression")
+args = vars(ap.parse_args())
+
+# initialize the class and color
+classes = "Basketball"
+COLORS = [(0, 204, 0)]
+
+# This pts deque is tracking the baskectball's center
+pts = deque(maxlen=args["buffer"])
+
+# I used the weightspath and configpath which I trained for basketball detection (bt colab)
+weightsPath = "yolov3_training_1000.weights"
+configPath =  "yolov3_testing.cfg"
+
+# load our YOLO object detector trained on yolov3_training_1000 (basketbll detection model)
+# and determine only the *output* layer names that we need from YOLO
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNet("yolov3_training_last.weights", "yolov3_testing.cfg")
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# initialize the video stream, pointer to output video file, and
+# frame dimensions
+vs = cv2.VideoCapture(args["input"])
+writer = None
+(W, H) = (None, None)
+
+# try to determine the total number of frames in the video file
+try:
+	prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() \
+		else cv2.CAP_PROP_FRAME_COUNT
+	total = int(vs.get(prop))
+	print("[INFO] {} total frames in video".format(total))
+
+# an error occurred while trying to determine the total
+# number of frames in the video file
+except:
+	print("[INFO] could not determine # of frames in video")
+	print("[INFO] no approx. completion time can be provided")
+	total = -1
+
+# loop over frames from the video file stream
+while True:
+	# read the next frame from the file
+	(grabbed, frame) = vs.read()
+
+	# if the frame was not grabbed, then we have reached the end
+	# of the stream
+	if not grabbed:
+		break
+
+	# if the frame dimensions are empty, grab them
+	if W is None or H is None:
+		(H, W) = frame.shape[:2]
+
+	# construct a blob from the input frame and then perform a forward
+	# pass of the YOLO object detector, giving us our bounding boxes
+	# and associated probabilities
+	blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
+		swapRB=True, crop=False)
+	net.setInput(blob)
+	start = time.time()
+	layerOutputs = net.forward(ln)
+	end = time.time()
+
+	# initialize our lists of detected bounding boxes, confidences,
+	# and class IDs, respectively
+	boxes = []
+	confidences = []
+	classIDs = []
+
+	# loop over each of the layer outputs
+	for output in layerOutputs:
+		# loop over each of the detections
+		for detection in output:
+			# extract the class ID and confidence (i.e., probability)
+			# of the current object detection
+			scores = detection[5:]
+			classID = np.argmax(scores)
+			confidence = scores[classID]
+
+			# filter out weak predictions by ensuring the detected
+			# probability is greater than the minimum probability
+			if confidence > args["confidence"]:
+				# scale the bounding box coordinates back relative to
+				# the size of the image, keeping in mind that YOLO
+				# actually returns the center (x, y)-coordinates of
+				# the bounding box followed by the boxes' width and
+				# height
+				box = detection[0:4] * np.array([W, H, W, H])
+				(centerX, centerY, width, height) = box.astype("int")
+
+				# use the center (x, y)-coordinates to derive the top
+				# and and left corner of the bounding box
+				x = int(centerX - (width / 2))
+				y = int(centerY - (height / 2))
+
+				# update our list of bounding box coordinates,
+				# confidences, and class IDs
+				boxes.append([x, y, int(width), int(height)])
+				confidences.append(float(confidence))
+				classIDs.append(classID)
+
+	# apply non-maxima suppression to suppress weak, overlapping
+	# bounding boxes
+	idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
+		args["threshold"])
+
+	# ensure at least one detection exists
+	if len(idxs) > 0:
+		# loop over the indexes we are keeping
+		for i in idxs.flatten():
+			# extract the bounding box coordinates
+			(x, y) = (boxes[i][0], boxes[i][1])
+			(w, h) = (boxes[i][2], boxes[i][3])
+
+			# draw a bounding box rectangle and label on the frame
+			color = [int(c) for c in COLORS[classIDs[i]]]
+
+			# Since I want to circle the basketball in the frame, I will define the center and radius for it.
+			center = (int(x+(w/2)), int(y+(h/2)))
+			radius = int(w/2)
+			cv2.circle(frame, center, radius, color, 2)
+			text = "{}: {:.4f}".format(classes,
+				confidences[i])
+			cv2.putText(frame, text, (x, y - 5),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+			# update the points deque
+			pts.appendleft(center)
+
+	# loop over the set of tracked points -> draw a tracking tail
+	for i in range(1, len(pts)):
+		# if either of the tracked points are None, ignore them
+		if pts[i - 1] is None or pts[i] is None:
+			continue
+
+		# otherwise, compute the thickness of the line and
+		# draw the connecting lines
+		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+		cv2.line(frame, pts[i - 1], pts[i], (0, 128, 255), thickness)
+
+	# check if the video writer is None
+	if writer is None:
+		# initialize our video writer
+		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+		writer = cv2.VideoWriter(args["output"], fourcc, 30,
+			(frame.shape[1], frame.shape[0]), True)
+
+		# some information on processing single frame
+		if total > 0:
+			elap = (end - start)
+			print("[INFO] single frame took {:.4f} seconds".format(elap))
+			print("[INFO] estimated total time to finish: {:.4f}".format(
+				elap * total))
+
+	# write the output frame to disk
+	writer.write(frame)
+
+# release the file pointers
+print("[INFO] cleaning up...")
+writer.release()
+vs.release()
